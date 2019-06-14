bb1a0c4cd30bd24d5db0d65bd632c82e47b09fa4
Bogdan Falk <bogdan37falk@gmail.com>
2019-06-14 13:16:00 +0300

    new db vars

--- config/config.js
+++ config/config.js
@@ -4,13 +4,13 @@ dotenv.config()
 module.exports = {
   port: process.env.PORT || 1337,
   db: {
-    database: process.env.DB_NAME || 'board_db',
-    user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASS || '',
+    database: process.env.DB_NAME || 'heroku_ba9289c47662677',
+    user: process.env.DB_USER || 'b7587e587674a8',
+    password: process.env.DB_PASS || '79199bcc',
     options: {
       dialect: process.env.DIALECT || 'mysql',
-      host: process.env.HOST || 'localhost',
-      port: process.env.DB_PORT || '3307'
+      host: process.env.HOST || 'eu-cdbr-west-02.cleardb.net',
+      port: process.env.DB_PORT || '3306'
     }
   }
 }
