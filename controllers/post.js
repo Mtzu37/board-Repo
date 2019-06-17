a7d579bc1eb97b29ec24a70107067b6ad35cdf47
Bogdan Falk <bogdan37falk@gmail.com>
2019-06-17 21:38:25 +0300

    another try final version

--- controllers/post.js
+++ controllers/post.js
@@ -51,7 +51,7 @@ const {
 
     }).then(user => {
       let flag = false;
-      if(user !== null || user !== undefined || user.passHash !== null )
+      if(user !== null || user !== undefined )
         flag = bcrypt.compareSync(password, user.passHash);
 
        if(flag){
